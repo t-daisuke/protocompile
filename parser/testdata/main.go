fe9094b78329d3be7fff027005cf0554b5dc97b7
t-daisuke <t.daisuke.64@gmail.com>
2024-08-28 21:21:08 +0900

    テストように作成

---
+++ parser/testdata/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/bufbuild/protocompile/parser"
+	"github.com/bufbuild/protocompile/reporter"
+)
+
+func main() {
+	// ファイルを読み込む
+	data, err := ioutil.ReadFile("./parser/testdata/largeproto.proto")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	// パーサーハンドラを作成
+	handler := reporter.NewHandler(nil)
+
+	// ASTに変換
+	fileNode, err := parser.Parse("largeproto.proto", bytes.NewReader(data), handler)
+	if err != nil {
+		fmt.Println("Error parsing file:", err)
+		return
+	}
+
+	// ASTをダンプ
+	_, err = parser.ResultFromAST(fileNode, true, handler) //
+	if err != nil {
+		fmt.Println("Error converting AST to result:", err)
+		return
+	}
+
+	fmt.Println("AST has been dumped to hoge.proto")
+}
